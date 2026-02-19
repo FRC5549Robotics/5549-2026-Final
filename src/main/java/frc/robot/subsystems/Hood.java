0e021d21a8a3ef1813fce5af8012c50ac4e522ce
Gryphon5549 <firstteam5549@gmail.com>
2026-02-18 21:28:07 -0500

    New Hood

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -4,6 +4,8 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,7 +15,8 @@ import edu.wpi.first.math.MathUtil;
 
 public class Hood extends SubsystemBase{
 
-    private final PositionVoltage positionRequest = new PositionVoltage(0).withEnableFOC(true);
+    private final MotionMagicVoltage positionRequest = new MotionMagicVoltage(0).withEnableFOC(true);
+    private final TorqueCurrentFOC homingRequest = new TorqueCurrentFOC(-8);
 
     private double hoodTarget;
 
@@ -23,20 +26,25 @@ public class Hood extends SubsystemBase{
     public Hood(){
         HoodMotor = new TalonFX(Constants.HOOD_MOTOR_ID, "lil clanker");
         HoodMotorConfig = new TalonFXConfiguration();
+
         HoodMotorConfig.CurrentLimits.StatorCurrentLimit = 60;
         HoodMotorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
         HoodMotorConfig.CurrentLimits.SupplyCurrentLimit = 25;
         HoodMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
 
-        HoodMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        HoodMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
         HoodMotorConfig.Slot0.kP = 20;
         HoodMotorConfig.Slot0.kI = 0;
         HoodMotorConfig.Slot0.kD = 0.2;
+        HoodMotorConfig.Slot0.kG = 0.35; //some gravity PID thing
 
         HoodMotorConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.05;
 
         HoodMotor.getConfigurator().apply(HoodMotorConfig);
+
+        HoodMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 4; // cap on velocity
+        HoodMotorConfig.MotionMagic.MotionMagicAcceleration = 8; // cap on acceleration
     }
 
     private double degreesToMotorRot(double deg) {
@@ -45,7 +53,7 @@ public class Hood extends SubsystemBase{
 
     public double getHoodPosition() {
         double pos = HoodMotor.getPosition().getValueAsDouble();
-        pos = pos/5*(184/11) + 22.3 + 45; //switch to degrees, just trust, don't change
+        pos = pos/5*(184/11) + 67.35; //switch to degrees, just trust, don't change
         return pos;
     }
 
@@ -56,17 +64,13 @@ public class Hood extends SubsystemBase{
         double motorRot = degreesToMotorRot(targetDeg);
         HoodMotor.setControl(positionRequest.withPosition(motorRot));
     }
-    
-    public void HoodOff() {
-        HoodMotor.set(0.0);
-    }
 
     public void hoodDownSlow() {
-        HoodMotor.set(-0.25);
+        HoodMotor.setControl(homingRequest);
     }
 
     public void stop() {
-        HoodMotor.set(0.0);
+        HoodMotor.setControl(positionRequest.withPosition(HoodMotor.getPosition().getValueAsDouble()));
     }
 
     public void zeroEncoder() {
@@ -74,7 +78,7 @@ public class Hood extends SubsystemBase{
     }
 
     public boolean atBottom() {
-        return Math.abs(HoodMotor.getVelocity().getValueAsDouble()) < 0.05;
+        return Math.abs(HoodMotor.getStatorCurrent().getValueAsDouble()) > 20;
     }
 
     @Override
