3b3f96e1c7789716acc4fceea60b7ef1e21ae9a0
Gryphon5549 <firstteam5549@gmail.com>
2026-04-23 15:52:42 -0400

    broken :(

--- src/main/java/frc/robot/commands/PassCommand.java
+++ src/main/java/frc/robot/commands/PassCommand.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
@@ -148,6 +147,13 @@ public class PassCommand extends Command {
    public void end(boolean interupted) {
        shooter.off();
        belt.off();
+       //if (allowAutoPivot.getAsBoolean()) {
+           //intake.shooting();
+       //
+      
+  
+   //}
+       drivetrain.stopDriving();
    }
 
 
